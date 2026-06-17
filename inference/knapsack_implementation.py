aebee21c5a289f692176c5b4a1ce25a6b8d10130
Leonardo Vilela Cardoso <lvcardoso@sga.pucminas.br>
2026-06-16 23:26:52 -0300

    Update knapsack_implementation.py

--- inference/knapsack_implementation.py
+++ inference/knapsack_implementation.py
@@ -2,45 +2,42 @@
 # author: Bhavya Jain
 # link: https://github.com/wulfebw/algorithms/blob/master/scripts/dynamic_programming/knapsack.py
 
+import numpy as np
+
+
 def knapSack(W, wt, val, n):
-	""" Maximize the value that a knapsack of capacity W can hold. You can either put the item or discard it, there is
-	no concept of putting some part of item in the knapsack.
+    wt  = np.array(wt,  dtype=np.int32)
+    val = np.array(val, dtype=np.float32)
 
-	:param int W: Maximum capacity -in frames- of the knapsack.
-	:param list[int] wt: The weights (lengths -in frames-) of each video shot.
-	:param list[float] val: The values (importance scores) of each video shot.
-	:param int n: The number of the shots.
-	:return: A list containing the indices of the selected shots.
-	"""
-	K = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+    K = np.zeros((n + 1, W + 1), dtype=np.float32)
 
-	# Build table K[][] in bottom up manner
-	for i in range(n + 1):
-		for w in range(W + 1):
-			if i == 0 or w == 0:
-				K[i][w] = 0
-			elif wt[i - 1] <= w:
-				K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
-			else:
-				K[i][w] = K[i - 1][w]
+    capacities = np.arange(W + 1, dtype=np.int32)
 
-	selected = []
-	w = W
-	for i in range(n, 0, -1):
-		if K[i][w] != K[i - 1][w]:
-			selected.insert(0, i - 1)
-			w -= wt[i - 1]
+    for i in range(1, n + 1):
+        w_i = wt[i - 1]
+        v_i = val[i - 1]
 
-	return selected
+        K[i] = K[i - 1]
+
+        mask      = capacities >= w_i
+        prev_caps = np.maximum(capacities - w_i, 0)          # clamp to avoid negative index
+        candidate = v_i + K[i - 1, prev_caps]
+        K[i]      = np.where(mask, np.maximum(K[i], candidate), K[i])
+
+    selected = []
+    w = W
+    for i in range(n, 0, -1):
+        if K[i, w] != K[i - 1, w]:
+            selected.insert(0, i - 1)
+            w -= wt[i - 1]
+
+    return selected
 
 
 if __name__ == "__main__":
-	pass
-	""" Driver program to test above function
-	val = [4, 4, 2, 2, 2, 4]
-	wt =  [2, 2, 1, 1, 1, 2]
-	W = 7
-	n = len(val)
-	selected = knapSack(W, wt, val, n)
-	print(selected)
-	"""
+    val      = [4, 4, 2, 2, 2, 4]
+    wt       = [2, 2, 1, 1, 1, 2]
+    W        = 7
+    n        = len(val)
+    selected = knapSack(W, wt, val, n)
+    print("Selected shot indices:", selected)   # expected: [0, 1, 5] or equivalent optimal set
